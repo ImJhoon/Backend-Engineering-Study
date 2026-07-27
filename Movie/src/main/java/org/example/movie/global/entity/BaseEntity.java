5e98071fd4ad9d288a347e7f1fffc32d25493403
wlsdn020416 <wlsdn020416@gmail.com>
2026-07-27 23:52:09 +0900

    feat: Security Config

--- Movie/src/main/java/org/example/movie/global/entity/BaseEntity.java
+++ Movie/src/main/java/org/example/movie/global/entity/BaseEntity.java
@@ -18,9 +18,9 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name ="created_at", nullable = false,  updatable = false)
-    private OffsetDateTime createdAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name ="updated_at", nullable = false)
-    private OffsetDateTime updatedAt;
+    private LocalDateTime updatedAt;
 }
