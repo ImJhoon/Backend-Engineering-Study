db3f0e32c9b6417a4acfb00bc72995d59480c435
wlsdn020416 <wlsdn020416@gmail.com>
2026-07-27 23:33:57 +0900

    feat: auth controller

---
+++ Movie/src/main/java/org/example/movie/domain/auth/service/AuthService.java
@@ -0,0 +1,37 @@
+package org.example.movie.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.movie.domain.auth.dto.request.SignupRequest;
+import org.example.movie.domain.auth.dto.response.SignupResponse;
+import org.example.movie.domain.member.entity.Member;
+import org.example.movie.domain.member.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SignupResponse signup(SignupRequest request) {
+
+        if (memberRepository.existsByEmail(request.email())){
+            throw new IllegalArgumentException("이미 사용중인 이메일입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        Member member = Member.create(
+                request.email(),
+                encodedPassword,
+                request.nickName()
+        );
+
+        Member savedMember = memberRepository.save(member);
+        return SignupResponse.from(savedMember);
+    }
+}
